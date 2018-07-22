409b13b880696635bfc8973280f61ff83e2cad60
elbobgamer10 <41314846+elbobgamer10@users.noreply.github.com>
2018-07-22 04:51:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -666,21 +666,4 @@ client.on('guildMemberAdd', member => {
 
 
 
-client.on("message", async message => {
-           let args = message.content.split(' ').slice(1)
-    const fs = require('fs-extra');
-  let newautorole = JSON.parse(fs.readFileSync("./autorole.json", "utf8"));
-if(message.content.startsWith(prefix + "setautorole")){
- if(!message.guild.member(message.author).hasPermission("ADMINISTRATOR")){return message.reply('**\`ADMINISTRATOR\`لا توجد لديك رتبة`**').catch(console.error);
-    } else {
-     if(!args.join(' ')) return message.channel.send("** اكتب اسم الرتبه**")
-     newautorole[message.guild.id] = {"autorole": args.join(" ")};
-     message.channel.send("لقد تم تفعيل اوتو رول`"+ args.join(" ") + "`👌");
-     fs.writeFile("./autorole.json", JSON.stringify(newautorole), (err) => {if (err) console.error(err);});
-   }
-}
-
-   });
-
-
 	  client.login(process.env.BOT_TOKEN);
