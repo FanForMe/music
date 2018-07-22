d4e99f7f1294163ae163bcc445d81d4fc41196fb
elbobgamer10 <41314846+elbobgamer10@users.noreply.github.com>
2018-07-22 03:12:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -573,14 +573,5 @@ function isYoutube(str) {
 
 
 
-client.on('voiceStateUpdate', (old, now) => {
-  const channel = client.channels.get('470392948291272706');
-  const currentSize = channel.guild.members.filter(m => m.voiceChannel).size;
-  const size = channel.name.match(/\[\s(\d+)\s\]/);
-  if (!size) return channel.setName(`Voice Online: ${currentSize}`);
-  if (currentSize !== size) channel.setName(`Voice Online: ${currentSize}`);
-});
-
-
 
 	  client.login(process.env.BOT_TOKEN);
