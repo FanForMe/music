bb33880ed53c0c4bb6bc2092a5f07104610cc683
elbobgamer10 <41314846+elbobgamer10@users.noreply.github.com>
2018-07-22 05:35:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -664,39 +664,49 @@ client.on('guildMemberAdd', member => {
     });
 
 
-
-
-if (message.content.startsWith(prefix - "help")) {
 client.on('message', message => {
-        message.react(" الاوامر العامة ")
-     let embed = new Discord.RichEmbed()")
-.setThumbnail(message.author.avatarURL)
-.addField('     **!bc** ' ,' **لخاصية البرودكاست** ')
-.addField('     **!id**  ' ,' **لظهور الايدي** ')
-.addField('     **!invites** ' , '**لمعرفة سويت كم انفايت**') 
-.addField('     **!clear** ' ,' **لمسح الشات** ')
-.addField('     **!server** ' ,' **معلومات عن السيرفر** ')
-.addField('     **!credits** ' ,' **لمعرفة فلوسك** ')	
-.addField('     **!daily** ' ,' **لأخذ فلوس كل يوم** ')	
-.addField('     **!trans {@mention}** ' ,' **لإرسال فلوس لأي حد تمنشنه** ')			    
-.setColor('#7d2dbe')
-  message.channel.sendEmbed(embed);
+if (message.content.startsWith(prefix + 'help')) {
+    let pages = ['هنا المكتوب بالصفحه الاولي','page2','page3']
+    let page = 1;
+
+    let embed = new Discord.RichEmbed()
+    .setColor('RANDOM')
+    .setFooter(`Page ${page} of ${pages.length}`)
+    .setDescription(pages[page-1])
+
+    message.channel.sendEmbed(embed).then(msg => {
+
+        msg.react('◀').then( r => {
+            msg.react('▶')
+
+
+        const backwardsFilter = (reaction, user) => reaction.emoji.name === '◀' && user.id === message.author.id;
+        const forwardsFilter = (reaction, user) => reaction.emoji.name === '▶' && user.id === message.author.id;
+
+
+        const backwards = msg.createReactionCollector(backwardsFilter, { time: 20000});
+        const forwards = msg.createReactionCollector(forwardsFilter, { time: 20000});
+
+
+
+        backwards.on('collect', r => {
+            if (page === 1) return;
+            page--;
+            embed.setDescription(pages[page-1]);
+            embed.setFooter(`Page ${page} of ${pages.length}`);
+            msg.edit(embed)
+        })
+        forwards.on('collect', r => {
+            if (page === pages.length) return;
+            page++;
+            embed.setDescription(pages[page-1]);
+            embed.setFooter(`Page ${page} of ${pages.length}`);
+            msg.edit(embed)
+        })
+        })
+    })
     }
-client.on('message', message => {
-            if (message.content.startsWith(prefix - "help")) {
-        message.react("اوامر الميوزك")
-     let embed = new Discord.RichEmbed()
-.setThumbnail(message.author.avatarURL)
-.addField('     **!join** ' ,' **لدخول البوت الروم** ')	
-.addField('     **!play** ' ,' **لتشفيل اغنية** ')
-.addField('     **!skip** ' ,' **لتغيير الأغنية** ')	     
-.addField('     **!pause** ' ,' **لإيقاف الأغنية مؤقتًا** ')	
-.addField('     **!resume** ' ,' **لتشفيل البوت** ')	
-.addField('     **!stop** ' ,' **لإيقاف الاغنية وخروج البوت من الروم** ')	
-.setColor('#7d2dbe')
-  message.channel.sendEmbed(embed);
-    };
-
+});
 
 
 
