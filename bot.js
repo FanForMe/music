5d74871e11cf023c5516b9fc76d39c63842dc5aa
elbobgamer10 <41314846+elbobgamer10@users.noreply.github.com>
2018-07-23 12:12:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -270,8 +270,8 @@ mentionned.send(` :credit_card: | Transfer Receipt \`\`\`You have received ${arg
 
 
 
-const Discord = require('discord.js');
-const { Client, Util } = require('discord.js');
+
+
 const client = new Discord.Client();
 const { PREFIX, GOOGLE_API_KEY } = require('./config');
 const YouTube = require('simple-youtube-api');
