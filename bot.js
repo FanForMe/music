a50bd8febe020128b5ef21389ed384b927528bc8
elbobgamer10 <41314846+elbobgamer10@users.noreply.github.com>
2018-07-24 07:15:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1168,4 +1168,12 @@ client.on('message' , message => {
 
 
 
+client.on('message' , message => {
+    if(message.content.includes === 'مين ؟') {
+    message.channel.send(`افسح التنين :joy:`);
+    }
+     });
+
+
+
 client.login(process.env.BOT_TOKEN)
