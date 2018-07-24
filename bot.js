882c34b93e6c1856d057455c1b6d290e243c48da
elbobgamer10 <41314846+elbobgamer10@users.noreply.github.com>
2018-07-24 18:48:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,7 +29,6 @@ client.on('message', message => {
 
 
  message.author.sendMessage(`
- **
 **ELBobBot** By: ELBobGamer ツ ᵖᵍˣᵖᵒ ♥#0881
 
 ╭━━━┳╮╱╱╭━━╮╱╱╱╭╮╱╭━━╮╱╱╱╭╮
@@ -1149,6 +1148,27 @@ if (command == "embed") {
 
 });
 
+
+
+
+
+
+client.on('message', message => {
+    if (message.content.startsWith("!avatar")) {
+        var mentionned = message.mentions.users.first();
+    var x5bzm;
+      if(mentionned){
+          var x5bzm = mentionned;
+      } else {
+          var x5bzm = message.author;
+          
+      }
+        const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setImage(`${x5bzm.avatarURL}`)
+      message.channel.sendEmbed(embed);
+    }
+});
 
 
 
