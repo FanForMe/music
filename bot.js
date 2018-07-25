cc5ee10520703189a2147a0e3383a52599e46525
elbobgamer10 <41314846+elbobgamer10@users.noreply.github.com>
2018-07-25 11:14:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1175,4 +1175,33 @@ client.on('message', message => {
 
 
 
+
+
+client.on('message', message => {
+    if (message.content.includes('discord.gg')){      //شيل المسافه
+                        if(!message.channel.guild) return message.reply ('')
+                    if (!message.member.hasPermissions(['MANAGE_MESSAGES'])){
+       message.channel.send('kick <@' + message.author.id + '>')
+       message.delete() 
+       }
+    }
+          if (message.content.startsWith("kick")) {
+             if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply();
+             var member= message.mentions.members.first();
+             member.ban().then((member) => {
+                 message.channel.sendMessage("", {embed: {
+                 author: {
+                 },
+                 title: 'بسبب النشر ' + member.displayName + ' تم طرد',
+                 color: 490101,
+                 }
+               });
+           }
+         ) 
+       }
+   });
+
+
+
+
 client.login(process.env.BOT_TOKEN)
