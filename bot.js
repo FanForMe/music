8125f151fadd419f73356abf27f08546d9f385f8
elbobgamer10 <41314846+elbobgamer10@users.noreply.github.com>
2018-07-25 13:24:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -458,12 +458,6 @@ const queue = new Map();
 
 });
 
-client.on('warn', console.warn);
-
-client.on('error', console.error);
-
-client.on('ready', () => console.log('Yo this ready!'));
-
 // client.on('disconnect', () => console.log('I just disconnected, making sure you know, I will reconnect now...'));
 
 // client.on('reconnecting', () => console.log('I am reconnecting now!'));
