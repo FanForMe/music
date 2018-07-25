20669e3fea6033018fa1e66813bc352f91e09d80
elbobgamer10 <41314846+elbobgamer10@users.noreply.github.com>
2018-07-25 19:00:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -448,11 +448,11 @@ mentionned.send(` :credit_card: | Transfer Receipt \`\`\`You have received ${arg
 
 
 
-const { PREFIX, GOOGLE_API_KEY } = require('./config.json');
+
 const YouTube = require('simple-youtube-api');
 const ytdl = require('ytdl-core');
 
-const youtube = new YouTube(GOOGLE_API_KEY);
+const youtube = new YouTube(AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8);
 
 const queue = new Map();
 
@@ -466,7 +466,7 @@ console.log('Logging into discord..');
 
 client.on('message', async msg => { // eslint-disable-line
 	if (msg.author.bot) return undefined;
-	if (!msg.content.startsWith(PREFIX)) return undefined;
+	if (!msg.content.startsWith(prefix)) return undefined;
 
 	const args = msg.content.split(' ');
 	const searchString = args.slice(1).join(' ');
@@ -474,7 +474,7 @@ client.on('message', async msg => { // eslint-disable-line
 	const serverQueue = queue.get(msg.guild.id);
 
 	let command = msg.content.toLowerCase().split(" ")[0];
-	command = command.slice(PREFIX.length)
+	command = command.slice(prefix.length)
 
 	if (command === "play") {
 		const voiceChannel = msg.member.voiceChannel;
