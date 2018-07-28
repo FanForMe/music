d2cc5f52a982d7c31e2c99d4e58172ca2cbeb3e8
elbobgamer10 <41314846+elbobgamer10@users.noreply.github.com>
2018-07-28 11:10:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1255,4 +1255,30 @@ client.on('guildMemberAdd', member=> {
 
 
 
+
+
+
+
+const developers = ["283691332633886720"]
+const adminprefixe = "-";
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith(adminprefix + 'setavatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
+});
+
+
+
+
+
+
+
 client.login(process.env.BOT_TOKEN)
