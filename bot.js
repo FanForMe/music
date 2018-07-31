a188a949ae52c02e3d205073397dd595b1d71fd0
elbobgamer10 <41314846+elbobgamer10@users.noreply.github.com>
2018-08-01 00:39:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1285,7 +1285,22 @@ if (message.content.startsWith(adminprefix + 'setavatar')) {
 
 
 
-
+client.on('voiceStateUpdate', (u, member) => {
+  var parent = '473982220600606731';
+  var channel = '473982309884755968';
+  if(member.voiceChannel === null || member.voiceChannel !== member.guild.channels.get(channel)) return console.log(`${member.user.username}'s channel isnt the needed one.`);
+  member.guild.createChannel(`${member.user.username}`, 'voice').then(c => {
+    if(!c) return;
+    c.setParent(parent);
+    member.setVoiceChannel(c);
+    setInterval(() => {
+      if(!c) return;
+      if(c.members.size === 0) {
+        c.delete();
+      }
+    }, 30000);
+  });
+});
 
 
 
