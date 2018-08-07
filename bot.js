db4d7a9eb47257b639165c6759bd67998cc3d623
elbobgamer10 <41314846+elbobgamer10@users.noreply.github.com>
2018-08-08 00:40:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1587,4 +1587,28 @@ client.on('message', message => {
 
 
 
+client.on('message', message =>{
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+    let prefix = '!!';
+
+if(cmd === `${prefix}suggest`) {
+    var suggestMessage = message.content.substring(8)
+    let suggestEMBED = new Discord.RichEmbed()
+    .setColor(3447003)
+    .setTitle("New suggest just added!!")
+    .setDescription(`**${suggestMessage}**`)
+    .setFooter(`Suggested By : ${message.author.tag}`);
+    message.delete().catch(O_o=>{}) 
+    let suggests = message.guild.channels.find(`name`, "Suggests");
+    if (!suggests) return message.channel.send("You should make A **suggests** channel!")
+    suggests.send(suggestEMBED);
+}
+
+});
+
+
+
+
 client.login(process.env.BOT_TOKEN)
