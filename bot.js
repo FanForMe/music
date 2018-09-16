1ff5b8ba9bb4c73a88f4b81ccb3454902728185d
elbobgamer10 <41314846+elbobgamer10@users.noreply.github.com>
2018-09-16 14:39:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1990,7 +1990,29 @@ if(!message.channel.guild) return;
 
 
 
-
+client.on('ready',async () => {
+  console.log(client.user.username);
+  try {
+    const config = {
+      name: "Rainbow", // اسم الرتبة
+      guildid: "489424581799706624", // اي دي السيرفر
+      sec: 2 // عدد الثواني
+    };
+    let guild = client.guilds.get(config.guildid);
+    let role = guild.roles.find(role => role.name === config.name);
+    let sec = config.sec * 1000;
+    if(!guild) return console.warn("Unkown guild.");
+    if(!role) return console.warn("Unkown role");
+    if(role.position >= guild.members.get(client.user.id).highestRole.position) return console.warn("Bot highest role must be above rainbow role");
+    setInterval(() => {
+      role.edit({ 
+      color: "RANDOM"
+    });
+    }, sec);
+  } catch(e) {
+    console.error(e);
+  }
+});
 
 
 
