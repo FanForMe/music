262a2d1aa1e127aee078a6a4bce4378d2865be25
elbobgamer10 <41314846+elbobgamer10@users.noreply.github.com>
2018-09-17 19:44:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1978,46 +1978,20 @@ if(!message.channel.guild) return;
 
 
 
-client.on("message", message => {
+
+const RainClient = require("rainbow-role")
+const rain = new RainClient("!", "Disco", 10, 5, false)
+
+//prefix (string) => البرفكس الي تبيه يكون للكوماند => default is "!"
+//roleName (string) => اسم الرول الي تبيه يكون حق ال rainbow => default is "Rainbow"
+//size (number) => كم عدد الالوان الي تبيه => default is 10
+//speed(number) => سرعة ال rainbow بالثواني => default is 60
+//logging(boolean ) => هل تبي يطلعلك بالكونسل لوق كل ما يتغير اللون؟ => default is false
  
-  function discoRole() {
-    let random = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-    roles.forEach((role) => {
-      let theRole = message.guild.roles.find("name", role);
-      theRole.edit({color: random}).catch(e => {
-        return message.channel.send(":x: **Error:** The role you specified in the `config.json` is either not a role on this server, or his a role higher than the highest role that I have.");
-      });
-    });
-  }
- 
-  if(message.content.startsWith(prefix + "startdisco")) {
-    if(allowedUsers.includes(message.author.id)) {
-    setInterval(() => { discoRole(); }, config.ms);
-    message.channel.send("```css\nDiscoing...```");
-    message.channel.send("Please make sure you read the README, you could get IP banned from discord because of ratelimits.");
-  } else {
-    message.reply(`You do not have permission to disco. If you have downloaded this bot off of github please go to the config.json and add your user ID to the "allowedUsers" value.`);
-  }
-} else
- 
-if(message.content.startsWith(prefix + "stopdisco")) {
-  if(allowedUsers.includes(message.author.id)) {
-  message.channel.send("I've stopped discoing.");
-  setTimeout(() => { console.log(process.exit(0)); }, 300);
-} else {
-  message.reply(`You do not have permission to disco. If you have downloaded this bot off of github please go to the config.json and add your user ID to the "allowedUsers" value.`);
-  }
-}
- 
-});
-const config = require('config.json');
- 
-const allowedUsers = config.allowedUsers;
-const roles = config.roleToDisco;
- 
-client.on("ready", () => {
-    console.log("Disco role bot online! Created by i am toast.");
-});
+//يمديك تستخدم اي من ال parmas في كودك ...
+
+rain.rainbow(client) // على حسب , اذا كنت معرفه client او bot
+
 
 
 
