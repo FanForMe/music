28db41fb39f7b9aee2a6e97ecff7849d2373db0f
elbobgamer10 <41314846+elbobgamer10@users.noreply.github.com>
2018-09-17 23:49:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2786,7 +2786,7 @@ client.on('message', message => {
 var embed = new Discord.RichEmbed()
 .setColor('RANDOM')
  .setThumbnail(message.author.avatarURL)
-.addField('SLFSTR BOT' ,
+.addField('ELBobBot' ,
 `${Za7f[Math.floor(Math.random() * Za7f.length)]}`)
 message.channel.sendEmbed(embed);
 console.log('[38ab] Send By: ' + message.author.username)
